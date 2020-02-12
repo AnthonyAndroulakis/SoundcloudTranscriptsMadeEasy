bacab6a8007df67b9603fd6e00b87a901c8ea236
Anthony Androulakis <aandroulakis@zoho.com>
2020-02-12 14:13:52 -0500

    Update parsetranscript.py

--- parsetranscript.py
+++ parsetranscript.py
@@ -23,7 +23,7 @@ def run(filename): #example input: transcript.en.csv
 		raise TranscriptError("LENGTH needs to be greater than last time mark.")
 
 	#create txt file with html-version of transcript
-	htmltext = '<iframe  id="musicframe" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+tracknum+'&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'
+	htmltext = '<iframe id="musicframe" width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+tracknum+'&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'
 	htmltext += '<br><br>'
 	htmltext += '<div class="btntoggle"><button class="tablinks" onclick="openInfo(event, \'btndetails\')">Details</button><button id="defaultOpen" class="tablinks" onclick="openInfo(event, \'btntranscript\')">Transcript</button></div>'
 	htmltext += '<div class="tabcontent" id="btndetails"><hr align="left">'+description+'</div>'
